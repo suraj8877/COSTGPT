a3d87a8b9ec27515af9546b2034ed9cb612d6a40
Suraj <surajbiswas2011@gmail.com>
2025-06-21 01:48:21 +0530

    adding wait

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://costgptpro.dev.codebuddy.review',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'COSTGPT/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     defaultCommandTimeout: 10000,   
     pageLoadTimeout: 60000,    
     setupNodeEvents(on, config) {
@@ -10,5 +12,3 @@ module.exports = defineConfig({
     },
   },
 });
-
-
---
+++ cypress/support/e2e.js
@@ -0,0 +1,4 @@
+/**
+ * This is the Cypress support file.
+ * Add global configuration and behavior here.
+ */
