9f643bad825f0c373f74e1592c269d76fb430d30
Suraj <surajbiswas2011@gmail.com>
2025-06-21 00:34:12 +0530

    config changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://costgptpro.dev.codebuddy.review',
+    defaultCommandTimeout: 10000,   
+    pageLoadTimeout: 60000,    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+});
